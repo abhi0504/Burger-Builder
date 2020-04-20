e25d9650ce92f4f6216ea9fb9c620a0f993ce6b4
Abhishek Sharma <sharmaash876@gmail.com>
2020-04-20 19:56:16 +0530

    app update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
-import Layout from './components/Layout/Layout'
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div>
-      <Layout>
-      <BurgerBuilder />
-      </Layout>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
-    )
+    );
   }
 }
 
