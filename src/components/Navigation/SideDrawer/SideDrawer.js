ccff32cb1c5fa5fa698105209c7fa67519d2fd27
Abhishek Sharma <sharmaash876@gmail.com>
2020-04-20 19:58:05 +0530

    Sidedraw Added

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auxilary from '../../../hoc/aux/auxilary';
+
+const sideDrawer = ( props ) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+    return (
+        <Auxilary>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Auxilary>
+    );
+};
+
+export default sideDrawer;
