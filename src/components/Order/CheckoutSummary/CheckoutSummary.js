23d76376e327beb1485af46d2a199a8be887b94d
Abhishek Sharma <sharmaash876@gmail.com>
2020-05-08 18:59:46 +0530

    Checkout feature

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,26 +1,24 @@
-import React , {Component} from "react"
-import Burger from'../../Burger/Burger';
-import Button from '../../UI/Button'
-import classes from './checkoutSummary.css'
+import React from 'react';
 
-const checkoutSummary = (props) => 
-{
-    return
-    (
-        <div>
-          <h1>We hope that it taste's well</h1>
-          <div style = {{width: '300px' , height : '300px' , margin: 'auto'}}>
-            <Burger ingredients={props.ingredients} />
-          </div>
-          <Button 
-          btnType='Danger'
-          clicked>CANCEL</Button>
-          <Button 
-          btnType='Success'
-          clicked>CONTINUE</Button>
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.css';
+
+const checkoutSummary = (props) => {
+    return (
+        <div className={classes.CheckoutSummary}>
+            <h1>We hope it tastes well!</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Burger ingredients={props.ingredients}/>
+            </div>
+            <Button
+                btnType="Danger"
+                clicked>CANCEL</Button>
+            <Button
+                btnType="Success"
+                clicked>CONTINUE</Button>
         </div>
-    )
-   
+    );
 }
 
-export default checkoutSummary
+export default checkoutSummary;
