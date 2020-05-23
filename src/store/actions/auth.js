d2a73f9aa4febbd43a0c24b37b31c4d33ab5a6bd
Abhishek Sharma <sharmaash876@gmail.com>
2020-05-23 21:22:40 +0530

    Auth setup

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
 
 export const authStart = () => {
     return {
@@ -20,8 +21,27 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password , isSignup) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email : email , 
+            password : password,
+            returnSecureToken : true
+        }
+        let url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDWD3u8IAG9EoPAHEM-YOOn9WN2YbD685g"
+        if (!isSignup) 
+        {
+            url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDWD3u8IAG9EoPAHEM-YOOn9WN2YbD685g"
+        }
+        axios.post( url , authData)
+        .then(response => {
+            console.log(response);
+            dispatch(authSuccess(response.data));
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch(authFail());
+        })
     };
 };
